5159dad11fdc836f35bd03103ccd3c65c8b46c7b
Aqib1422 <mraqibshafique@gmail.com>
2024-06-25 15:52:05 +0500

    Todo

--- src/todolist/Singletodo.jsx
+++ src/todolist/Singletodo.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdDelete } from "react-icons/md";
-const Singletodo = ({item,remove,index}) => {
+const Singletodo = ({ item, remove, index }) => {
+    const [check, setCheck] = useState(false)
   return (
       <>
           <div className=" col-lg-5 col-10 mx-auto p-3 shadow my-2 rounded-3 d-flex flex-row align-items-center justify-content-between">
               <h5 >{item}</h5>
+              <div className="d-flex flex-row align-items-center justify-content-center">
               <button className='bg-white border-0' onClick={()=>remove(index)}>
                   <MdDelete className='text-danger'  />
-              </button>
+                  </button>
+              <button className='form-check bg-white border-0'>
+                      <input type="checkbox"
+                  onChange={()=>setCheck(true)}
+                      />
+                  </button>
+                
+                  </div>
               
           </div>
       </>
